005e4aef3a9a72596f5e58f629d25dd0d25a9e2d
J-VE <jason.vanepps@gmail.com>
2023-02-03 11:55:12 -0500

    Add project files.

---
+++ Password Strength Checker/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Strength_Checker
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Password Strength Checker/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    public class Tools
+    {
+        public Tools()
+        {
+        }
+
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+
+        public static void ContainsTest()
+        {
+            string loudWord = "LASDAD";
+            string quietWord = "pssst";
+            string mixedWord = "lkaAWEkasfkEW";
+
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine("Running \"tests...\"");
+            Console.WriteLine($"Should be true: {Contains(loudWord, uppercase)}");
+            Console.WriteLine($"Should be false: {Contains(loudWord, lowercase)}");
+            Console.WriteLine($"Should be false: {Contains(quietWord, uppercase)}");
+            Console.WriteLine($"Should be true: {Contains(quietWord, lowercase)}");
+            Console.WriteLine($"Should be true: {Contains(mixedWord, uppercase)}");
+            Console.WriteLine($"Should be true: {Contains(mixedWord, lowercase)}");
+        }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+
+                Console.WriteLine("Enter a password: ");
+                string password = Console.ReadLine();
+                int score = 0;
+
+                if (password.Length > 8)
+                {
+                    score++;
+                }
+                if (Tools.Contains(password, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+                {
+                    score++;
+                }
+                if (Tools.Contains(password, "abcdefghijklmnopqrstuvwxyz"))
+                {
+                    score++;
+                }
+                if (Tools.Contains(password, "1234567890"))
+                {
+                    score++;
+                }
+                if (Tools.Contains(password, "!@#$%^&*()_-+={[}]|:;<,>.?`~"))
+                {
+                    score++;
+                }
+                Console.WriteLine($"Your password strength is: {score}");
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
